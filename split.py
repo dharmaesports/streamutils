a0008185a9bda2b1ce2b58ce1f79f071e203226b
Dharma Esports <dharmaesports@gmail.com>
2019-10-22 23:06:30 -0700

    added actual values for DITP1

--- split.py
+++ split.py
@@ -1,25 +1,27 @@
 # each int in this array is a second to split on; will start at the first entry,
 # so it should probably start with 0; it will go from the last entry to the end
 # of the video
-splits_in_seconds = [0, 60, 120]
+splits_in_seconds = [0, 552, 1166, 1682, 2180, 2667, 3275, 4093, 5400, 6023, 6622, 7454, 8017, 8659, 9840, 10591, 11185]
+descriptors = ['Vincessant Vs Soup Nazi', 'Jaded Vs Soup Nazi', 'Panda Vs Vincessant', 'Bladewise Vs Rosebud', 'Panda Vs Soup Nazi', 'Panda Vs Jaded', 'Panda Vs Dacky', 'Vincessant Vs Blink', 'Luigi Ka-Master Vs Melo', 'Bladewise Vs Kramer', 'Bladewise Vs Vincessant', 'Dacky Vs Melo', 'Panda Vs Luigi Ka-Master', 'Panda Vs Bladewise', 'Luigi Ka-Master Vs Dacky', 'Panda Vs Luigi Ka-Master', 'Panda Vs Bladewise']
 
 # input file name
-input_filename = 'DITP2MeleeSinglesPools.mkv'
+input_filename = 'Diamond in the puff 1.mkv'
 
 # output parameters; output files will follow the pattern:
 # "{output_base} 001 - {descriptor_item}{output_extension}"
 # descriptors must be same length as splits_in_seconds
-output_base = 'DITP2 Melee Doubles'
-descriptors = ['Team A Vs Team B', 'Team C vs Team D', 'Team A vs Team D']
+output_base = 'DITP1 Melee Singles'
 output_extension = '.mp4'
 
 
 import ffmpeg
 
 num_splits = len(splits_in_seconds)
+print(num_splits)
 last_split_index = num_splits - 1
 
 for split_index in range(0, num_splits):
+    print(split_index)
     vid_index = split_index + 1
     vid_index_string = str(vid_index).zfill(3) # 3 is number of digits in the index output
 
